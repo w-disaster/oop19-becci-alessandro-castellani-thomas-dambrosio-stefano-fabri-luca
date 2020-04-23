03355035d41de94a5d223b706df0545dd0c8f5d0
Stefano <stefano.dambrosio4@studio.unibo.it>
2020-04-23 15:27:40 +0200

    Changed stage size to 1280x720

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -5,25 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import viewmenu.SceneBuilder;
-import viewmenu.SceneBuilderImpl;
 
 /**
  * This class represent the Main class of the JavaFX-based application.
  */
 public final class Main extends Application {
 
-    private static final int SCENE_WIDTH = 600;
-    private static final int SCENE_HEIGHT = 400;
+    private static final int SCENE_WIDTH = 1280;
+    private static final int SCENE_HEIGHT = 720;
 
     @Override
     public void start(final Stage stage) throws Exception {
-    	SceneBuilder sceneBuild = new SceneBuilderImpl(SCENE_WIDTH, SCENE_HEIGHT, "layouts/main.fxml");
+        final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/main.fxml"));
+        final Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
         // Stage configuration
-        stage.setTitle("Quoridor2D - Menu");
-        stage.setScene(sceneBuild.getScene());
-        stage.setMinWidth(SCENE_WIDTH);
-        stage.setMinHeight(SCENE_HEIGHT);
+        stage.setTitle("JavaFX - Complete Example");
+        stage.setScene(scene);
         stage.show();
     }
 
