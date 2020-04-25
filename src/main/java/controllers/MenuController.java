a5db17bae3d7280acda06dc19b777cb86f079435
unknown <alessandro.becci@studio.unibo.it>
2020-04-25 15:36:49 +0200

    added new buttons in menuController

--- src/main/java/controllers/MenuController.java
+++ src/main/java/controllers/MenuController.java
@@ -1,12 +1,28 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import jdk.jshell.spi.ExecutionControl.*;
 
 public class MenuController {
 
 	 @FXML
-	    public void exitOnClickHandler() {
-	        System.exit(0);
-	    }
-	
+    public void exitButtonPressHandler() {
+        System.exit(0);
+    }
+
+	 @FXML
+	 public void newGameButtonPressHandler() throws NotImplementedException {
+		 throw new NotImplementedException("Not Implemented Yet");
+	 }
+	 
+	 @FXML
+	 public void loadGameButtonPressHandler() throws NotImplementedException {
+		 throw new NotImplementedException("Not Implemented Yet");
+	 }
+	 
+	 @FXML
+	 public void leaderboardButtonPressHandler() throws NotImplementedException {
+		 throw new NotImplementedException("Not Implemented Yet");
+	 }
+	 
 }
