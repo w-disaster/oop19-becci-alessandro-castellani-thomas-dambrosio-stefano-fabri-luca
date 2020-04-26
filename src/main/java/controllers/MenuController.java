228b16079ed1da814cfa27fab63a366ad68ae14e
unknown <alessandro.becci@studio.unibo.it>
2020-04-26 15:59:46 +0200

    modified menuController for starting view

--- src/main/java/controllers/MenuController.java
+++ src/main/java/controllers/MenuController.java
@@ -1,18 +1,35 @@
 package controllers;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
 import jdk.jshell.spi.ExecutionControl.*;
+import viewmenu.SceneBuilder;
+import viewmenu.SceneBuilderImpl;
 
 public class MenuController {
 
-	 @FXML
+	 private static final double SCALING_RATE = 1.5;
+
+	@FXML
     public void exitButtonPressHandler() {
         System.exit(0);
     }
 
 	 @FXML
-	 public void newGameButtonPressHandler() throws NotImplementedException {
-		 throw new NotImplementedException("Not Implemented Yet");
+	 public void newGameButtonPressHandler(ActionEvent event) throws IOException {
+		 SceneBuilder sceneBuild = new SceneBuilderImpl(SCALING_RATE, "layouts/main/scene.fxml");
+		 
+		 Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		 stage.setTitle("Quoridor2D - Game");
+	     stage.setScene(sceneBuild.getScene());
+	     stage.sizeToScene();
+	     stage.show();
+	     stage.setMinWidth(stage.getWidth());
+	     stage.setMinHeight(stage.getHeight());
 	 }
 	 
 	 @FXML
