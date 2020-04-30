798870a027ac2e7cf6eb7c498b673164a37c7ced
unknown <castellani.thomas.iti@gmail.com>
2020-04-30 17:32:42 +0200

    Implemented movePlayer method, missing adjacent and noWall checks

--- src/main/java/controller/PlayerMoverImpl.java
+++ src/main/java/controller/PlayerMoverImpl.java
@@ -6,11 +6,22 @@ public class PlayerMoverImpl implements PlayerMover {
 
 	private StandardGame game = new StandardGameImpl();
 	private Barriers barriers = new BarriersImpl();
+	private Pair<Integer,Integer> playerPosition = this.game.getCurrentPlayer().getCoordinate();
+	private Pair<Integer,Integer> newPosition;
 	
 	@Override
-	public void movePlayer(Pair<Integer, Integer> position) {
-		// TODO Auto-generated method stub
-		
+	public void movePlayer(Pair<Integer, Integer> newPosition) {
+		this.newPosition = newPosition; //so i don't need to pass at all private methods a parameter
+		if (this.adjacent() && this.noWall()) {
+			this.playerPosition = newPosition;
+		}
 	}
-
+	
+	private boolean adjacent() {
+		return true;
+	}
+	
+	private boolean noWall() {
+		return true;
+	}
 }
