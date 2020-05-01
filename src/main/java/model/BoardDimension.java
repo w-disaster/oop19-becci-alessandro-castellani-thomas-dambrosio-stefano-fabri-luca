fba2ae207c1848e9fa8d98c3097352855d397ede
luca <luca.fabri@studio.unibo.it>
2020-05-01 11:57:20 +0200

    undo Singleton BoardDimension

---
+++ src/main/java/model/BoardDimension.java
@@ -0,0 +1,17 @@
+package model;
+
+public interface BoardDimension {
+	
+	/**
+	 * 
+	 * @return the dimension
+	 */
+	int get();
+	
+	/**
+	 * sets the specified dimension
+	 * @param dimension
+	 */
+	void set(int dimension);
+
+}
