3278deee5cda96e1983be94d8dccbc65b263c683
unknown <thomas.castellani@studio.unibo.it>
2020-05-15 16:23:32 +0200

    Now barriers are long two positions

--- src/main/java/controller/BarrierPlacerImpl.java
+++ src/main/java/controller/BarrierPlacerImpl.java
@@ -30,9 +30,16 @@ public class BarrierPlacerImpl extends MoveImpl implements BarrierPlacer {
 	public void placeBarrier(Coordinate position, BarrierType type) {
 		this.newBarrierPosition = position;
 		this.newBarrierType = type;
-		if (this.isEmptyPosition() && this.enoughBarriers() && this.noStall()) {
+		if (this.isEmptyPosition() && this.enoughBarriers() && this.checkPosition() && this.noStall()) {
 			this.players.getCurrentPlayer().setAvailableBarriers(this.players.getCurrentPlayer().getAvailableBarriers() - 1);
-			this.barriers.add(new BarrierImpl(position, type));
+			//to place barriers long 2 positions i have to add 2 barriers
+			if (type.equals(BarrierType.HORIZONTAL)) {
+				this.barriers.add(new BarrierImpl(position, type));
+				this.barriers.add(new BarrierImpl(new Coordinate(position.getX() + 1, position.getY()), type));
+			} else {
+				this.barriers.add(new BarrierImpl(position, type));
+				this.barriers.add(new BarrierImpl(new Coordinate(position.getX(), position.getY() + 1), type));
+			}
 			this.changeTurn();
 		} else {
 			System.out.println("Bad move! Still your turn!");
@@ -45,6 +52,17 @@ public class BarrierPlacerImpl extends MoveImpl implements BarrierPlacer {
 	
 	private boolean enoughBarriers() {
 		return this.players.getCurrentPlayer().getAvailableBarriers() > 0 ? true : false;
+	}
+	
+	private boolean checkPosition() {
+		//barriers are long 2 positions so they can't be placed where x or y are 'boardDimension'
+		if (this.newBarrierPosition.getX().equals(this.model.getBoardDimension() - 1)) {
+			return true;
+		}
+		if ( this.newBarrierPosition.getY().equals(this.model.getBoardDimension() - 1)) {
+			return true;
+		}
+		return false;
 	}
 	
 	private boolean noStall() {
