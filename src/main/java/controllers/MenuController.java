86c506145c6ff59cf3da5d2ffd744e13588c1d78
Alessandro Becci <alessandro.becci@studio.unibo.it>
2020-05-25 12:05:16 +0200

    now the menu starts the controller of the game

--- src/main/java/controllers/MenuController.java
+++ src/main/java/controllers/MenuController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.lang.ModuleLayer.Controller;
 
 import application.Main;
+import controller.StandardGameControllerImpl;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -16,6 +17,8 @@ import viewmenu.SceneChangerImpl;
 
 public class MenuController {
 	
+	StandardGameControllerImpl controller ;
+	
 	@FXML
     public void exitButtonPressHandler() {
         System.exit(0);
@@ -25,8 +28,9 @@ public class MenuController {
 	 public void newGameButtonPressHandler(ActionEvent event) throws IOException {
 		 SceneChanger sceneChange = new SceneChangerImpl(event);
 		 sceneChange.change("layouts/main/scene.fxml", "Quoridor2D - Game");
-		 UIController controller = new UIController();
-		 
+		 //UIController controller = new UIController();
+		 controller = new StandardGameControllerImpl();
+		 controller.newStandardGame("player1", "player2");
 	 }
 	 
 	 @FXML
